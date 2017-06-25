67bcba020af5a44b90fb26a8abe95516c5f7fbed
Jan Redmann <janredmann1@gmail.com>
2017-06-25 21:42:36 +0200

    Add relationships to Post and User

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['title', 'url', 'source_domain'];
+
+    public function postedBy()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
